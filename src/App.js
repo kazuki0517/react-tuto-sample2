a3c5623e022bf39a443f80037089b0c12f66ad18
kazuki0517 <kh2328pkh@gmail.com>
2023-02-26 11:25:21 +0900

    react tuto #5

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
+import React from 'react';
 import './App.css';
 
 function App() {
-  let items = [
-    {"names": "いちご", "price": "100"},
-    {"names": "りんご", "price": "150"},
-    {"names": "バナナ", "price": "230"}
-  ]
-
-  return (
-    <div className="App container">
-      <table className="table table-striped">
-        <tbody>
-          {items.map((value) => (
-          <tr>
-            <th scope="row">{value.names}</th>
-            <td>{value.price}</td>
-          </tr>
-          ))}
-        </tbody>
-      </table>
+  return(
+    <div className='container text-center'>
+          <Clock />
     </div>
+
   )
+}
+
+class Clock extends React.Component {
+  constructor(props){
+    super(props);
+    this.now = new Date();
+    this.state = {
+      time : `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
+    }
+    this.refresh = this.refresh.bind(this);
+  }
+
+  refresh() {
+    this.now = new Date();
+    this.setState((state) => ({
+      time : `${this.now.getHours()}:${this.now.getMinutes()}:${this.now.getSeconds()}`
+    }));
+  }
+  render() {
+    return <p onClick={this.refresh}>{this.state.time}</p>
+  }
 }
 
 export default App;
